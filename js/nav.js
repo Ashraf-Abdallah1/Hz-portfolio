84519d0380398bd2b7b61042935e969c9e0f9381
Ashraf_Abd <240163@.student.scalda.nl>
2025-09-02 17:16:03 +0200

    fixed the about me page

--- js/nav.js
+++ js/nav.js
@@ -2,4 +2,15 @@ fetch("nav.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("nav").innerHTML = data;
-    })
+
+
+        // het is voor    kleur van de link van de huidge pagina  (nodig omdat de nav met js wordt geladen )
+        const links = document.querySelectorAll('.navbar a');
+        const currentPage = window.location.pathname.split('/').pop();
+
+        links.forEach(link => {
+            if(link.getAttribute('href') === currentPage) {
+                link.classList.add('active');
+            }
+        });
+    });
